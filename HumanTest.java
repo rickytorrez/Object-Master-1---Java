5c0e5354c97a671dd1e603d9e95f9ec311f3b54a
Ricardo <torrez.ricky7@gmail.com>
2018-01-12 12:43:45 -0500

    initiall commit

---
+++ HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+    public static void main(String[] args){
+        Human x = new Human ("Wil");
+        Human y = new Human ("Kap");
+        
+        x.attack(y);
+    }
+}
